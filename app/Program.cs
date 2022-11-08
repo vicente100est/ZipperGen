c28d0efd003d76d0fc422a9b8ae2e63d84b9d83e
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-11-08 16:11:44 -0600

    Zipper Generator Release

---
+++ app/Program.cs
@@ -0,0 +1,47 @@
+﻿using NLog;
+using System;
+using System.IO.Compression;
+using zipper;
+
+namespace app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = null;
+            string target = null;
+
+            if (args == null || args.Length != 2)
+            {
+                System.Console.WriteLine("Uso: <fuente> <destino.zip>");
+                return;
+            }
+
+            source = args[0];
+            target = args[1];
+
+            ConfigureLog();
+
+            try
+            {
+                ZipperLogic.Zip(source, target);
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetCurrentClassLogger().Error(ex);
+            }
+        }
+
+        public static void ConfigureLog()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "logs.txt" };
+
+
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            LogManager.Configuration = config;
+        }
+    }
+}
---
+++ zipper/ZipperLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace zipper
+{
+    public class ZipperLogic
+    {
+        public static void Zip(string source, string target)
+        {
+            ZipFile.CreateFromDirectory(source, target);
+        }
+    }
+}
